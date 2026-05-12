c2a42398341fc9bfb985a7f41b224f1a96864126
유정우 <jaguar0615@cau.ac.kr>
2026-05-12 23:31:24 +0900

    feat: Inverse Volatility 자산 배분 최적화 구현 (DB-06)

--- data-pipeline/pipeline/fetch_price_history.py
+++ data-pipeline/pipeline/fetch_price_history.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sqlalchemy import create_engine
 from datetime import datetime
 import time
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import DB_URL
 
 engine = create_engine(DB_URL)
--- data-pipeline/pipeline/sync_stocks.py
+++ data-pipeline/pipeline/sync_stocks.py
@@ -1,5 +1,8 @@
 import FinanceDataReader as fdr
 from sqlalchemy import create_engine, text
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import DB_URL
 
 engine = create_engine(DB_URL)
